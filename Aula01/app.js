23d900dbc6f4dd05b39de9c8795fd4729101bfcf
Tais575 <lara.tlda@gmail.com>
2019-08-08 20:36:32 -0300

    Validar cpf

--- Aula01/app.js
+++ Aula01/app.js
@@ -21,13 +21,28 @@ app.use('/cpf/:cpf', (req, res) => {
     let cpf = req.params.cpf;
     //Deixo pra vcs!
 
-    if(cpf.length == 14){
-        console.log("CPF válido");
-    }else{
-        console.log("CPF inválido");
-    }
+    let Soma;
+    let Resto;
+    Soma = 0;
+  if (cpf == "00000000000") return false;
+     
+  for (let i=1; i<=9; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+   
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(cpf.substring(9, 10)) ) return false;
+   
+  Soma = 0;
+    for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+   
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(cpf.substring(10, 11) ) ) return res.send(false);
+    return res.send(true);
+
+
     
-    res.send(cpf);
+    res.send();
 });
 
 app.listen(3000, () => console.log('Servidor iniciado'));
