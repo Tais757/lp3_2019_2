1f1c137f750b6437edde526e9e76b3e76df7ee71
Taís <lara.tlda@gmail.com>
2019-11-05 17:22:26 -0300

    Avaliação

--- Avaliacao_LP3/controllers/UsuarioController.js
+++ Avaliacao_LP3/controllers/UsuarioController.js
@@ -6,13 +6,14 @@ const controller = {
     recuperarItens: async (req, res) => {
         const { filtro } = req.body;
         const itens = await Item.find({ 
-            descricao: { '$regex': filtro, '$options': 'i' } 
+            nome: { '$regex': filtro, '$options': 'i' },
+            email: { '$regex': filtro, '$options': 'i' }
         });
         return res.json(itens);
     },
 
     salvar: (req, res) =>{
-        const { nome } = req.body;
+        const { nome, email, senha} = req.body;
         if (nome) {
             const usuario = req.body;
             Usuario
@@ -23,7 +24,46 @@ const controller = {
                 res.status(500).json({ mensagem: 'Erro ao tentar salvar o usuario'});
             });
         } else {
-            return res.status(400).json({ mensagem: 'Nome não encontrado'});
+            return res.status(400).json({ mensagem: 'Usuário não informado'});
+        }
+
+        if (email) {
+            const email = req.body;
+            Usuario
+            .create(email)
+            .then(salvaEmail => res.status(201).json(salvaEmail))
+            .catch(erro => {
+                console.log(erro);
+                res.status(500).json({ mensagem: 'Erro ao tentar salvar o e-mail do usuario'});
+            });
+        } else {
+            return res.status(400).json({ mensagem: 'E-mail de usuário não informado'});
+        }
+          
+        while (senha) {
+            const digito = senha;
+            const regex = /^(?=(?:.*?[A-Z]){1})(?=(?:.*?[0-9]){1})/;
+
+            if (digito.length < 8){
+                return res.status(400).json({ mensagem: 'A senha deve conter no mínimo 8 digitos!' });
+            } else if (!regex.exec(digito)){
+                return res.status(400).json({ mensagem: 'A senha deve conter no mínimo 1 letra maiúscula e 1 digito numérico!' });
+            } else if (senha === digito ){
+             return res.status(400).json({ mensagem: 'Senha diferente' });
+            }
+
+            if (senha){
+            const senha = req.body;
+            Usuario
+            .create(senha)
+            .then(salvaSenha => res.status(201).json(salvaSenha))
+            .catch(erro => {
+                console.log(erro);
+                res.status(500).json({ mensagem: 'Erro ao tentar salvar a senha de usuario'});
+            });
+        } else {
+            return res.status(400).json({ mensagem: 'Senha de usuário inválida'});
+        }
         }
     }
 };
